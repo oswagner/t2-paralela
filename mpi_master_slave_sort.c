aaf23725594946035af1ce833ad7fe54ca58319a
Wagner Santos <wosantos@thoughtworks.com>
2019-11-24 20:33:41 -0300

    teste filename

--- mpi_master_slave_sort.c
+++ mpi_master_slave_sort.c
@@ -23,19 +23,20 @@ void printArrayFile(int arr[], int size, char *hostname, int process_id)
     FILE *output_file;
     char filename[200];
 
-    // snprintf(filename, 200, "/out/array_%s_%d.txt", hostname, process_id);
+    snprintf(filename, 200, "./out/array_%s_%d.txt", hostname, process_id);
+    printf("%s", filename);
 
     // output_file = fopen(filename, "w+");
-    output_file = fopen("teste.txt", "w+");
-    int i;
-    fprintf(output_file, "%s\n\t", "Vetor final ordenado:");
-    for (i = 0; i < size; i++)
-    {
-        fprintf(output_file, "%d, ", arr[i]);
-    }
-    fprintf(output_file, "\n\n");
-    printf("\n\n");
-    fclose(output_file);
+    // // output_file = fopen("teste.txt", "w+");
+    // int i;
+    // fprintf(output_file, "%s\n\t", "Vetor final ordenado:");
+    // for (i = 0; i < size; i++)
+    // {
+    //     fprintf(output_file, "%d, ", arr[i]);
+    // }
+    // fprintf(output_file, "\n\n");
+    // printf("\n\n");
+    // fclose(output_file);
 }
 
 int main(int argc, char **argv)
